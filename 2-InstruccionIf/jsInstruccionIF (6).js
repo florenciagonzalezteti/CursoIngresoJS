a11c79f8cd1ccf5edcc54c946b2e17e744d8ae9f
alumno <alumno>
2019-02-16 11:28:59 -0300

    Clase de apoyo EJ 1-6 IF

--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -1,8 +1,41 @@
 function mostrar()
 {
-//tomo la edad  
+/* 
+var laEdad;
+var laEdad=edad.value;
 
+if(laEdad>=18)
+{
+	alert("Es mayor de edad.");
+}
 
+if(laEdad>=13 && laEdad=<17)
+{
+	alert("Es adolescente.");
+}
+
+if(laEdad<13)
+{
+	alert("Es niño/a.");
+}
+*/
+
+var laEdad;
+var laEdad=edad.value;
+
+if(laEdad>=18)
+{
+	alert("Es mayor de edad.");
+	
+	if(laEdad<13)
+		alert("Es un niño.");
+	else
+	{
+		alert("Es adolescente");
+	}
+
+	
+}
 
 
 }//FIN DE LA FUNCIÓN
